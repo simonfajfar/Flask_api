8318e1ee0457ca99aa04bf8fcc16823579911c99
simonfajfar <fajfarsimon@gmail.com>
2021-02-06 21:11:30 +0100

    Heroku upload.

--- main.py
+++ main.py
@@ -30,4 +30,4 @@ def stevilo_uporabnikov():
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app.run(debug=True, port=port)
+    app.run(debug=True, port=port, host='0.0.0.0')
