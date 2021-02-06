29adf28a5bfb8e8e353414959a5295aa6d5d7e84
simonfajfar <fajfarsimon@gmail.com>
2021-02-06 21:05:49 +0100

    Heroku upload.

---
+++ README.md
@@ -0,0 +1,18 @@
+# Testni projekt
+
+Tukaj sem naredil testni projekt za falsk.
+
+Todo list:
+ - flask (how to show list of dict, how to show list of elements to html)
+ - virtual enviroment python
+ - bootstrap, google material (css)
+ - pycharm
+ - requirements.txt
+ - git (remotes, commit, add, push, remote)
+ - github (REMOTE.md, markdown)
+ - heroku
+ - how servers work
+ - request, response GET, POST, PUT, DELETE
+ - how api works (routes, path)
+ - how to use external api (google api, facebook api, youtube api)
+ - ubuntu 
--- main.py
+++ main.py
@@ -3,10 +3,31 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
+count = 0
+uporabniki = [
+    {"ime": "Uros", "priimek": "Jarc"},
+    {"ime": "Uros", "priimek": "Jarc"},
+]
+
+@app.route("/", methods=['GET'])
 def index():
+    global count
+    count += 1
     return render_template('index.html', naslov= 'Živijo')
+
+@app.route("/biografija", methods=['GET'])
+def biografija():
+    global count
+    count += 1
+    return render_template('biografija.html')
+
+@app.route("/stevilo-uporabnikov", methods=['GET'])
+def stevilo_uporabnikov():
+    global count
+    global uporabniki
+    count += 1
+    return render_template('stevilo_uporabnikov.html', count = count, uporabniki = uporabniki)
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(debug=True, port=port)
