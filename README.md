0580a4e54bea0cefaf480b699d8fdf724755be2d
simonfajfar <fajfarsimon@gmail.com>
2021-02-24 20:41:03 +0100

    drugi alineji dodal besedilo

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ Tukaj sem naredil testni projekt za falsk.
 
 Todo list:
  - flask (how to show list of dict, how to show list of elements to html)
- - virtual enviroment python
+ - virtual enviroment python (trenutno delam)
  - bootstrap, google material (css)
  - requirements.txt
  - git (remotes, commit, add, push, remote)
--- main.py
+++ main.py
@@ -3,19 +3,17 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route("/", methods=['GET'])
 def index():
-    return render_template('index.html', naslov= 'Živijo')
+    return render_template('index.html', naslov='Živijo')
+
 
 @app.route("/biografija", methods=['GET'])
 def biografija():
     return render_template('biografija.html')
 
+
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(debug=True, port=port, host='0.0.0.0')
-
-
-
-
-
